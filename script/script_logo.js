3e27057aac8d7fe331c3011c48880a0109acac6f
PPSXD <raynerchung888@gmail.com>
2024-11-01 17:44:44 +0800

    first commit

--- script/script_logo.js
+++ script/script_logo.js
@@ -1,4 +1,4 @@
-$('#mark-1').hover(function() {
+  $('#mark-1').hover(function() {
     $(this).addClass('fa-flip');
     }, function() {
     $(this).removeClass('fa-flip'); 
@@ -8,4 +8,8 @@ $('#mark-1').hover(function() {
     }, function() {
     $(this).removeClass('fa-flip'); 
     });
-    
+    $('#mark-3').hover(function() {
+      $(this).addClass('fa-flip');
+      }, function() {
+      $(this).removeClass('fa-flip'); 
+      });
